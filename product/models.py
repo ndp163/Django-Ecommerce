abba100f790eb62db2b851f049fb1ea48c8387b0
= <=>
2020-07-19 14:27:10 +0700

    updated-19-7

--- product/models.py
+++ product/models.py
@@ -42,3 +42,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(blank=True, max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
